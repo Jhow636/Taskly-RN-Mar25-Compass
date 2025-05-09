f548d2dd6d4738f958077b02c22cce490c3b9609
Gabriel Arthur <gabrielarthur11333@gmail.com>
2025-05-09 07:24:48 -0300

    feat(navigation): add UserEdit screen to MainStackNavigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -21,6 +21,7 @@ import Terms from '../screens/Terms';
 import Menu from '../screens/Menu';
 import Preferencies from '../screens/Preferencies/Index';
 import AvatarUpdate from '../screens/AvatarUpdate';
+import UserEdit from '../screens/UserEdit';
 
 const AuthStack = createNativeStackNavigator<AuthStackParamList>();
 const MainStack = createNativeStackNavigator<MainStackParamList>();
@@ -95,6 +96,7 @@ function MenuStackNavigator() {
       <MainStack.Screen name="Preferencies" component={Preferencies} />
       <MainStack.Screen name="DarkMode" component={PreferencesScreen} />
       <MainStack.Screen name="AvatarUpdate" component={AvatarUpdate} />
+      <MainStack.Screen name="UserEdit" component={UserEdit} />
     </MainStack.Navigator>
   );
 }
